ee72168f3e4bf4d0c79199764843deef115470d3
AlbertWebs <albertmuhatia@gmail.com>
2024-05-24 12:30:23 +0300

    Update

---
+++ resources/views/front/homespage-section.blade.php
@@ -0,0 +1,135 @@
+	<!-- Start Protect Area -->
+		<section class="protect-area pt-100 pb-70">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-6">
+						<div class="protect-content">
+							<div class="protect-title">
+								<span>Cyber Security Protect</span>
+								<h2>Protect your website, web server, and web application for helping you stay away from the hacker</h2>
+							</div>
+
+							<div class="row">
+								<div class="col-lg-6 col-sm-6">
+									<div class="single-challenges overly-one">
+										<div class="overly-two">
+											<i class="flaticon-database"></i>
+											<h3>Database Security</h3>
+											<p>The threat to data has never been greater. </p>
+											<span class="flaticon-database"></span>
+										</div>
+									</div>
+								</div>
+
+								<div class="col-lg-6 col-sm-6">
+									<div class="single-challenges overly-one">
+										<div class="overly-two">
+											<i class="flaticon-application"></i>
+											<h3>Application Security</h3>
+											<p>We Enable you Securely build, deploy and iterate applications.</p>
+											<span class="flaticon-application"></span>
+										</div>
+									</div>
+								</div>
+
+								<div class="col-lg-6 col-sm-6">
+									<div class="single-challenges overly-one">
+										<div class="overly-two">
+											<i class="flaticon-security"></i>
+											<h3>Web Security</h3>
+											<p>We ensure your website is up to date with neccessary security protocols</p>
+											<span class="flaticon-security"></span>
+										</div>
+									</div>
+								</div>
+
+								<div class="col-lg-6 col-sm-6">
+									<div class="single-challenges overly-one">
+										<div class="overly-two">
+											<i class="flaticon-security-1"></i>
+											<h3>Server Security</h3>
+											<p>We provide you with hardening and optimization in many different areas of server</p>
+											<span class="flaticon-security-1"></span>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+
+					<div class="col-lg-6">
+						<div class="protect-img">
+							<img style="border-radius:20px;" src="{{asset('theme/assets/img/protect-img.jpg')}}" alt="Image">
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+		<!-- Eed Protect Area -->
+
+		<!-- Start Testimonials Area -->
+		<section class="testimonials-area ptb-100 jarallax" data-jarallax='{"speed": 0.3}'>
+			<div class="container">
+				<div class="testimonials">
+					<span>Our Portfolio</span>
+
+					<div class="testimonials-slider owl-carousel owl-theme">
+						<div class="testimonials-item">
+							<i class="flaticon-quote"></i>
+							<p>
+                                End Evaluation IAPF Food security and Nutrition Project, Somalia
+                            </p>
+
+							{{-- <ul>
+								<li>
+									<i class="bx bxs-star"></i>
+								</li>
+								<li>
+									<i class="bx bxs-star"></i>
+								</li>
+								<li>
+									<i class="bx bxs-star"></i>
+								</li>
+								<li>
+									<i class="bx bxs-star"></i>
+								</li>
+								<li>
+									<i class="bx bxs-star"></i>
+								</li>
+							</ul> --}}
+
+							<h3>Concern Worldwide</h3>
+							<span>April 30th -June 10th, 2022 </span>
+						</div>
+						<div class="testimonials-item">
+							<i class="flaticon-reliability"></i>
+							<p>
+                                Final Evaluation of Triple Nexus Pilot Project
+                            </p>
+
+							{{-- <ul>
+								<li>
+									<i class="bx bxs-star"></i>
+								</li>
+								<li>
+									<i class="bx bxs-star"></i>
+								</li>
+								<li>
+									<i class="bx bxs-star"></i>
+								</li>
+								<li>
+									<i class="bx bxs-star"></i>
+								</li>
+								<li>
+									<i class="bx bxs-star"></i>
+								</li>
+							</ul> --}}
+
+							<h3>Nexus Platform</h3>
+							<span>February 20th -March15th, 2022</span>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+		<!-- Eed Testimonials Area -->
